1e409f963cb255352a53b3873c4d7d4a32992b86
Johannes Weiß <johannes.weiss@bromium.com>
2015-03-23 11:02:38 +0000

    initial checkin

---
+++ undefined.swift
@@ -0,0 +1,28 @@
+/**
+ * `undefined()` pretends to be able to produce a value of any type `T` which can
+ * be very useful whilst writing a program. It happens that you need a value
+ * (which can be a function as well) of a certain type but you can't produce it
+ * just yet. However, you can always temporarily replace it by `undefined()`.
+ *
+ * Inspired by Haskell's
+ * [undefined](http://hackage.haskell.org/package/base-4.7.0.2/docs/Prelude.html#v:undefined).
+ *
+ * Invoking `undefined()` will crash your program.
+ *
+ * Some examples:
+ *
+ *  - `let x : String = undefined()
+ *  - `let f : String -> Int? = undefined("string to optional int function")
+ *  - `return undefined() /* in any function */`
+ *  - `let x : String = (undefined() as Int -> String)(42)`
+ *  - ...
+ *
+ * What a crash looks like:
+ *
+ * `fatal error: undefined: main.swift, line 131`
+ *
+ */
+func undefined<T>(_ hint:String="", file:StaticString=__FILE__, line:UWord=__LINE__) -> T {
+    let message = hint == "" ? "" : " \(hint)"
+    fatalError("undefined\(message)", file:file, line:line)
+}
